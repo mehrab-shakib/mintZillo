b480feac64676c779c27767a259ce19aadfec4ec
mehrab-shakib <fatscorpieeth@gmail.com>
2024-02-15 12:55:01 +0600

    Setting up frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
+import {useEffect, useState} from 'react'; 
+import {ethers} from 'ethers'; 
+
+// importing abi 
+import RealEstate from './contract_abi/RealEstate.json'
+import Escrow from './contract_abi/Escrow.json'; 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
+       
+       <h1>Welcome to Mint-Zillo!</h1>
       </header>
     </div>
   );
