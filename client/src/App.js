8b0eb5b7525f1985787d12e0b95bdcb8b0141d3b
mehrab-shakib <fatscorpieeth@gmail.com>
2024-02-19 00:59:07 +0600

    frontend updated added navigation search and logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,62 @@
-import {useEffect, useState} from 'react'; 
-import {ethers} from 'ethers'; 
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
 
-// importing abi 
-import RealEstate from './contract_abi/RealEstate.json'
-import Escrow from './contract_abi/Escrow.json'; 
-import './App.css';
+// importing abi
+import RealEstate from "./contract_abi/RealEstate.json";
+import Escrow from "./contract_abi/Escrow.json";
+
+// importing components
+import "./components/Navigation";
+import "./components/Home";
+import "./components/Search";
+
+// importing config
+import config from "./config.json";
+import Navigation from "./components/Navigation";
+import Search from "./components/Search"
 
 function App() {
+  const [account, setAccount] = useState(null);
+
+  //connecting Metamask;
+  const loadChaindata = async () => {
+    const provider = new ethers.BrowserProvider(window.ethereum);
+
+    window.ethereum.on('accountsChanged', async () => {
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const account = ethers.getAddress(accounts[0])
+      setAccount(account);
+    })
+  };
+
+  useEffect(() => {
+    loadChaindata();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-       
-       
-       <h1>Welcome to Mint-Zillo!</h1>
-      </header>
+    <div>
+      <Navigation account={account} setAccount={setAccount}/>
+      <Search />
+      <div className="cards__section">
+        <h2>Homes For you.</h2>
+        <hr/> 
+        <div className="cards">
+          <div className="card">
+            <div className="card__image">
+              <img src="" alt="Home" />
+            </div>
+            <div className="card__info">
+              <h4>1 ETH</h4>
+              <p>
+                <strong>1 </strong>bds | 
+                <strong> 2 </strong>ba | 
+                <strong> 3 </strong>sqft | 
+              </p>
+            </div>
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 }
---
+++ client/src/components/Navigation.js
@@ -0,0 +1,51 @@
+import { ethers } from "ethers";
+import logo from "../assets/logo2.svg";
+const Navigation = ({ account, setAccount }) => {
+    const connectWalletHandler = async () => {
+        const accounts = await window.ethereum.request({
+            method: "eth_requestAccounts"
+          });
+
+          setAccount(accounts[0]); 
+    }
+
+  return (
+    <nav>
+      <ul className="nav__links">
+        <li>
+          <a href="#">Buy</a>
+        </li>
+        <li>
+          <a href="#">Rent</a>
+        </li>
+        <li>
+          <a href="#">Sell</a>
+        </li>
+      </ul>
+
+      <div className="nav__brand">
+        <img src={logo} alt="Logo" />
+        
+      </div>
+      {account ? (
+                <button
+                    type="button"
+                    className='nav__connect'
+                >
+                    {account.slice(0, 6) + '...' + account.slice(38, 42)}
+                </button>
+            ) : (
+                <button
+                    type="button"
+                    className='nav__connect'
+                    onClick={connectWalletHandler}
+                >
+                    Connect
+                </button>
+            )}
+      
+    </nav>
+  );
+};
+
+export default Navigation;
